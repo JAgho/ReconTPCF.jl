9dbdd57c6a9e69736f38bdddac41c846b342b7dc
JAgho <GholamJA@cardiff.ac.uk>
2021-02-02 16:27:23 +0000

    Added dependencies

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
 using ReconTPCF
 using Test
 
+ReconTPCF.loadim("uniform_circles.png")
+
 @testset "ReconTPCF.jl" begin
     # Write your tests here.
-ReconTPCF.loadim("uniform_circles.png")
+#ReconTPCF.loadim("uniform_circles.png")
 end
